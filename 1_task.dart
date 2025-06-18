8cf60d1e845e123b7ad38b748710d82054e0bb25
Zozulia Evhenii <metamorf.40k@gmail.com>
2025-06-18 20:32:48 +0300

    feat: add task3 function to check store purchase conditions based on money and store status

--- 1_task.dart
+++ 1_task.dart
@@ -4,7 +4,13 @@ void main(List<String> args) {
   const width = 18.4;
   const length = 30.9;
   calcRectangle(width, length);
+
   proccedTask2();
+
+  var hasMoney = Random().nextBool();
+  var isStoreOpen = Random().nextBool();
+  proccedTask3(hasMoney, isStoreOpen);
+  
 }
 
 void calcRectangle(double width, double length) {
@@ -24,4 +30,16 @@ void proccedTask2() {
   print('Is Number greater than 50: $isGreater');
   print('Is Number less than 100: $isLess');
   print('Is Number odd by 5: $isOddBy5');
+}
+
+void proccedTask3(bool hasMoney, bool isStoreOpen){
+  print('Has money: $hasMoney');
+  print('Is store open: $isStoreOpen');
+  if(hasMoney && isStoreOpen){
+    print('You can buy something');
+  }else if(!hasMoney || !isStoreOpen){
+    print('Wait for the store to open or get some money');
+  }else if (!hasMoney && !isStoreOpen){
+    print('You do not have money and the store is closed');
+  }
 }
