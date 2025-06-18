b45e6b1650520d04de1214a62b994e54b85c1374
Zozulia Evhenii <metamorf.40k@gmail.com>
2025-06-18 20:09:48 +0300

    feat: implement rectangle calculator with area and perimeter calculations

---
+++ 1_task.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var width = 18.4;
+  var length = 30.9;
+  var area = width * length;
+  var perimeter = 2 * (width + length);
+  print('Area: $area');
+  print('Perimeter: $perimeter');
+  var maximumSquares = area~/1;
+  print('Maximum squares: $maximumSquares');
+}
