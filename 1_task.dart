83c5e327d9750d6f010704b2c6dbf76dc77a4204
Zozulia Evhenii <metamorf.40k@gmail.com>
2025-06-18 20:17:07 +0300

    feat: add random number from 0 to 100 checks

--- 1_task.dart
+++ 1_task.dart
@@ -1,10 +1,27 @@
+import 'dart:math';
+
 void main(List<String> args) {
-  var width = 18.4;
-  var length = 30.9;
+  const width = 18.4;
+  const length = 30.9;
+  calcRectangle(width, length);
+  proccedTask2();
+}
+
+void calcRectangle(double width, double length) {
   var area = width * length;
   var perimeter = 2 * (width + length);
   print('Area: $area');
   print('Perimeter: $perimeter');
   var maximumSquares = area~/1;
-  print('Maximum squares: $maximumSquares');
+}
+
+void proccedTask2() {
+  var number = Random().nextInt(100);
+  bool isGreater = number > 50;
+  bool isLess = number < 100;
+  bool isOddBy5 = number % 5 == 0;
+  print('Number: $number');
+  print('Is Number greater than 50: $isGreater');
+  print('Is Number less than 100: $isLess');
+  print('Is Number odd by 5: $isOddBy5');
 }
