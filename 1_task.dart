c5727370313f856a77c52695d7a6081780a021bd
Zozulia Evhenii <metamorf.40k@gmail.com>
2025-06-20 03:02:34 +0300

    feat: add task10 to print numbers with break and continue conditions

--- 1_task.dart
+++ 1_task.dart
@@ -31,6 +31,9 @@ void main(List<String> args) {
 
   print('${'-' * 40} Task 9 ${'-' * 40}');
   proceedTask9();
+
+  print('${'-' * 40} Task 10 ${'-' * 40}');
+  proceedTask10();
 }
 
 void calcRectangle(double width, double length) {
@@ -164,3 +167,15 @@ void proceedTask9() {
     i--;
   }
 }
+
+void proceedTask10() {
+  for (int i = 1; i <= 20; i++) {
+    if (i > 15) {
+      break;
+    } else if (i % 3 == 0) {
+      continue;
+    } else {
+      print(i);
+    }
+  }
+} 
