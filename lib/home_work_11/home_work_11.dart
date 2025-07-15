8c83bc9ca49f12f9c66a49508ff71906662f31d7
Zozulia Evhenii <metamorf.40k@gmail.com>
2025-07-15 22:26:51 +0300

    feat: implement homework 11 tasks for Futures. Moved tasks implementation from home_work_11.dart into hw_futeres.dart

--- lib/home_work_11/home_work_11.dart
+++ lib/home_work_11/home_work_11.dart
@@ -1,31 +1,17 @@
 import 'package:dart_learning/home_work_11/hw_futures.dart';
-import 'package:dart_learning/util.dart';
+import 'package:dart_learning/home_work_11/hw_streams.dart';
 
 runHW11() async {
-  printTaskDivider(11,1);
-  var name = await FuturedHomeWorkRunner.fetchName(Duration(seconds: 2));
-  print('Мене звати  $name');
+  
+  //Futures-----------------
+  await runTask1();
+  await runTask2();
+  await runTask3();
+  await runTask4();
+  await runTask5();
 
-  printTaskDivider(11,2);
-  var yearsOld = await FuturedHomeWorkRunner.fetchAge(Duration(milliseconds: 1500));
-  print('Мені $yearsOld ${yearsDeclension(int.parse(yearsOld))}');
-
-  printTaskDivider(11,3);
-
-  final stopwatch = Stopwatch();
-  stopwatch.start();
-  await FuturedHomeWorkRunner.fetchName(Duration(seconds: 2));
-  await FuturedHomeWorkRunner.fetchAge(Duration(milliseconds: 1500));
-  stopwatch.stop();
-  print('Занальний час виконання послідовного виконання: ${stopwatch.elapsedMilliseconds} ms');
-
-  printTaskDivider(11,4);
-  final stopwatch2 = Stopwatch();
-  stopwatch2.start();
-  await Future.wait([ FuturedHomeWorkRunner.fetchName(Duration(seconds: 2)), FuturedHomeWorkRunner.fetchAge(Duration(milliseconds: 1500))]);
-  stopwatch2.stop();
-  print('Занальний час виконання паралельного виконання: ${stopwatch2.elapsedMilliseconds} ms');
-
-  printTaskDivider(11,5);
-  await FuturedHomeWorkRunner.delayedCountdown(5);
+  //Streams-----------------
+  await runTask6();
+  await runTask7();
+  await runTask8();
 }
--- lib/home_work_11/hw_futures.dart
+++ lib/home_work_11/hw_futures.dart
@@ -1,3 +1,5 @@
+import 'package:dart_learning/util.dart';
+
 abstract class FuturedHomeWorkRunner {
   static Future<String> fetchName(Duration duration) async {
     return await Future.delayed(duration, () => "Yevhenii");
@@ -15,4 +17,51 @@ abstract class FuturedHomeWorkRunner {
     await Future.delayed(const Duration(seconds: 1), () => print('Старт!'));
     return 'Старт!';
   }
+}
+
+Future<void> runTask1() async {
+  printTaskDivider(11, 1);
+
+  var name = await FuturedHomeWorkRunner.fetchName(Duration(seconds: 2));
+  print('Мене звати  $name');
+}
+
+Future<void> runTask2() async {
+  printTaskDivider(11, 2);
+  var yearsOld = await FuturedHomeWorkRunner.fetchAge(
+    Duration(milliseconds: 1500),
+  );
+  print('Мені $yearsOld ${yearsDeclension(int.parse(yearsOld))}');
+}
+
+Future<void> runTask3() async {
+  printTaskDivider(11, 3);
+
+  final stopwatch = Stopwatch();
+  stopwatch.start();
+  await FuturedHomeWorkRunner.fetchName(Duration(seconds: 2));
+  await FuturedHomeWorkRunner.fetchAge(Duration(milliseconds: 1500));
+  stopwatch.stop();
+  print(
+    'Занальний час виконання послідовного виконання: ${stopwatch.elapsedMilliseconds} ms',
+  );
+}
+
+Future<void> runTask4() async {
+  printTaskDivider(11, 4);
+  final stopwatch2 = Stopwatch();
+  stopwatch2.start();
+  await Future.wait([
+    FuturedHomeWorkRunner.fetchName(Duration(seconds: 2)),
+    FuturedHomeWorkRunner.fetchAge(Duration(milliseconds: 1500)),
+  ]);
+  stopwatch2.stop();
+  print(
+    'Занальний час виконання паралельного виконання: ${stopwatch2.elapsedMilliseconds} ms',
+  );
+}
+
+Future<void> runTask5() async {
+  printTaskDivider(11, 5);
+  await FuturedHomeWorkRunner.delayedCountdown(5);
 }
