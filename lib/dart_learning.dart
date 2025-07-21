a0788270d22561ac2aef4e75c537c92d5dd5b226
Zozulia Yevhenii <metamorf.40k@gmail.com>
2025-07-21 21:57:35 +0300

    Revert "Dart homework 11"

--- lib/dart_learning.dart
+++ lib/dart_learning.dart
@@ -1,16 +1,14 @@
 // ignore_for_file: unused_import
 
-import 'package:dart_learning/home_work_11/home_work_11.dart';
 import 'package:dart_learning/home_work_6/home_work_6.dart';
 import 'package:dart_learning/home_work_8/home_work_8.dart';
 
 void main() {
   // runHW6();
-  // runHW8();
-  runHW11();
+  runHW8();
 }
 
 //for test case
 int calculate() {
-  return 14 * 3;
+  return 14*3;
 }
